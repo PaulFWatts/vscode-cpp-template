7ede44e25fbea278ad079b486025ed7235b68cc6
Paul Watts <paul.watts@me.com>
2024-11-07 16:28:32 +1100

    complete

---
+++ Proj2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num{100};
+    cout << "Hello from Proj2" << endl;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "You entered: " << num << endl;
+    return 0;
+}
